55b8318abea8f2dd5e172ea6cb49c2203a4afeef
Shinichi Okada <okada.shin@gmail.com>
2022-02-07 19:23:39 +0900

    Darkmode re-added. Installing with npm.

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,7 +1,7 @@
 <script>
   import "../app.css";
   import "flowbite/dist/flowbite.css";
-  // import { DarkMode } from "$lib/index";
+  import { DarkMode } from "$lib/index";
   import {
     Aside,
     Nav,
@@ -109,6 +109,7 @@
 </Aside>
 
 <main class="container mx-auto px-4 pt-4">
+  <DarkMode />
   <div class="mt-8 w-full">
     <slot />
   </div>
