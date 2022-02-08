b9175a014163d9bbf8f9af37e3d428b62cdf6602
Shinichi Okada <okada.shin@gmail.com>
2022-02-08 13:51:20 +0900

    Working on the top menu.

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -25,7 +25,7 @@
   let asideClass =
     "absolute w-auto bg-white pt-8 shadow-lg z-50 px-4 h-full bg-white dark:bg-gray-800";
   let headerClass =
-    "px-8 bg-white h-14 pt-3 text-gray-600 border-b-2 bg-white dark:bg-gray-800 dark:text-white dark:border-b-1";
+    "px-8 bg-white h-16 pt-3 text-gray-600 border-b-2 bg-white dark:bg-gray-800 dark:text-white dark:border-b-1";
   let siteName = "Svelte Flow";
   let navClass =
     "py-0 px-8 bg-white text-sm bg-white dark:bg-gray-800 dark:text-white";
@@ -34,7 +34,7 @@
   let siteClass = " pt-1.5 pl-12 sm:w-1/2";
   let siteText = "text-lg";
   let topli =
-    "block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50  md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-300 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white  dark:border-gray-700 text-lg z-50 bg-white dark:bg-gray-800 dark:border-b-0";
+    "block py-2 pr-4 pl-3 text-gray-700 hover:bg-gray-50  md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-300 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white text-lg z-50 bg-white dark:bg-gray-800 dark:border-0";
   let topul =
     "flex flex-col mt-4 md:flex-row md:space-x-8 md:mt-0 md:text-md md:font-medium pt-1 dark:bg-gray-800";
   let topMenuDiv =
