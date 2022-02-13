b3f7def52740de1c88ecf4299daa263c05cdfbbd
Shinichi Okada <okada.shin@gmail.com>
2022-02-14 06:48:40 +0900

    Changed to Getting started in the Index page link.

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -27,10 +27,10 @@
           <div class="mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-start">
             <div class="mt-3 sm:mt-0 sm:ml-3">
               <a
-                href="/modals"
+                href="/getting-started"
                 class="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-indigo-700 bg-indigo-100 hover:bg-indigo-200 md:py-4 md:text-lg md:px-10"
               >
-                Modal Live demo
+                Getting started
               </a>
             </div>
           </div>
