2ef3b64a28744ba0dfc7954322da022022baf50c
Shinichi Okada <okada.shin@gmail.com>
2022-02-18 16:17:19 +0900

    Demo-utterances: Install @codewithshin/svelte-utterances

--- src/routes/layouts/doc.svelte
+++ src/routes/layouts/doc.svelte
@@ -2,12 +2,12 @@
   import Prism from "prismjs";
   import "prismjs/themes/prism.css";
   import "prism-themes/themes/prism-holi-theme.css";
-  import Comments from "./Comments.svelte";
+  import { Utterances } from "@codewithshin/svelte-utterances";
 </script>
 
 <div class="max-w-3xl mx-auto pb-32">
   <div class="container mt-4 flex flex-wrap mx-auto pt-16">
     <slot />
-    <Comments />
+    <Utterances reponame="shinokada/svelte-flow" />
   </div>
 </div>
