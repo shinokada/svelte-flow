9f33c34cbe74fef5841bdde6cc4d2864df8b80c1
Shinichi Okada <okada.shin@gmail.com>
2022-02-18 14:54:04 +0900

    Demo-utterances: Comments module is working.

--- src/routes/layouts/doc.svelte
+++ src/routes/layouts/doc.svelte
@@ -2,10 +2,12 @@
   import Prism from "prismjs";
   import "prismjs/themes/prism.css";
   import "prism-themes/themes/prism-holi-theme.css";
+  import Comments from "./Comments.svelte";
 </script>
 
 <div class="max-w-3xl mx-auto pb-32">
   <div class="container mt-4 flex flex-wrap mx-auto pt-16">
     <slot />
+    <Comments />
   </div>
 </div>
